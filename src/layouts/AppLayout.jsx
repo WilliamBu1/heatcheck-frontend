ecda5c779730186d1338b510ba4cc9f4488823f6
William Bu <williambu1027@gmail.com>
2025-05-15 13:54:28 -0400

    home page

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Navbar from '../components/Navbar.jsx'; // Import Navbar
+import { Outlet } from 'react-router-dom'; // Ensure Outlet is imported
 // We don't necessarily need useAuth here if Navbar handles its own auth-based rendering
 // and AppLayout is only used for routes that should always try to show the Navbar.
 
 const AppLayout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-black">
+    <div className="flex flex-col min-h-screen"> {/* bg-black is typically inherited from MainLayout if MainLayout wraps this */}
       <Navbar /> {/* Navbar is rendered here */}
-      <main className="flex-grow text-white p-5 box-border">
-        {/* The p-5 might need adjustment based on global styles or content needs */}
-        {children}
+      <main className="flex-grow p-5"> {/* Added p-5 for padding around content */}
+        <Outlet /> {/* Child route elements (like HomePage) will render here */}
       </main>
       {/* You could add a Footer here later if needed */}
     </div>
